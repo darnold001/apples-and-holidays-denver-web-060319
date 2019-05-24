74530aa9fcbb0117c881341b7d097afc7cee043c
Devin Arnold <dbarnold321@gmail.com>
2019-05-24 02:00:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,6 @@ holiday_hash.each do |season, holiday|
     #include?("BBQ")
      bbq_h << holiday
     end
-  end
 puts bbq_h
 
   # return an array of holiday names (as symbols) where supply lists
